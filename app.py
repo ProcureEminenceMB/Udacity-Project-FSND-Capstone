cd28339692d0f5589dc23c9e992a1271c414d529
Michael Bigelow <procureeminence@gmail.com>
2020-08-16 11:21:02 -0400

    fix: Starter code inconsistent spacing

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+	# create and configure the app
+	app = Flask(__name__)
+	CORS(app)
 
-  return app
+	return app
 
 APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+	APP.run(host='0.0.0.0', port=8080, debug=True)
