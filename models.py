8e7ca6d2dcf6ee7bcbd036005aec9f1368a8c491
Michael Bigelow <procureeminence@gmail.com>
2020-08-16 14:11:36 -0400

    feat: Add actor and movie db classes

--- models.py
+++ models.py
@@ -22,24 +22,64 @@ def setup_db(app, database_path=database_path):
 	db.init_app(app)
 	db.create_all()
 
-
-'''
-Person
-Have title and release year
-'''
-class Person(db.Model):  
-	__tablename__ = 'People'
+# Classes
+class Actor(db.Model):
+	__tablename__ = 'actors'
 
 	id = Column(Integer, primary_key=True)
 	name = Column(String)
-	catchphrase = Column(String)
+	age = Column(Integer)
+	gender = Column(String)
 
-	def __init__(self, name, catchphrase=""):
+	def __init__(self, name, age,  gender):
 		self.name = name
-		self.catchphrase = catchphrase
+		self.age = age
+		self.gender = gender
+
+	def insert(self):
+		db.session.add(self)
+		db.session.commit()
+
+	def update(self):
+		db.session.commit()
+
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
 
 	def format(self):
 		return {
 			'id': self.id,
 			'name': self.name,
-			'catchphrase': self.catchphrase}
+			'age': self.age,
+			'gender': self.gender
+		}
+
+class Movie(db.Model):
+	__tablename__ = 'movies'
+
+	id = Column(Integer, primary_key=True)
+	title = Column(String)
+	release_date = Column(db.DateTime, nullable=False)
+
+	def __init__(self, title, release_date):
+		self.title = title
+		self.release_date = release_date
+
+	def insert(self):
+		db.session.add(self)
+		db.session.commit()
+
+	def update(self):
+		db.session.commit()
+
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
+
+	def format(self):
+		return {
+			'id': self.id,
+			'title': self.title,
+			'release date': self.release_date
+		}
