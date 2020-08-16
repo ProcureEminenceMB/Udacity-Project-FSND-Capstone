b41cbb8b30379c23f13fede5041ea5a765b21858
Michael Bigelow <procureeminence@gmail.com>
2020-08-16 15:11:46 -0400

    feat: Add auth requirements to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
+AUTH0_DOMAIN='udacity-coffee-project.us.auth0.com'
+API_AUDIENCE='udacity-fsnd-capstone-project-api'
+CLIENT_ID='9Fv6meuLEH1FZeCcMz1HmqdZTqvjawyu'
 JWT_SECRET='DerpyCode'
 LOG_LEVEL=DEBUG
