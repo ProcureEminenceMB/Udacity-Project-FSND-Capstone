148dbcb663ee45a07d5253ab06f9e7e1912ed496
Michael Bigelow <procureeminence@gmail.com>
2020-08-16 12:06:40 -0400

    fix: Starter code inconsistent spacing

--- models.py
+++ models.py
@@ -8,14 +8,14 @@ db = SQLAlchemy()
 
 '''
 setup_db(app)
-    binds a flask application and a SQLAlchemy service
+	binds a flask application and a SQLAlchemy service
 '''
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
+	app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+	db.app = app
+	db.init_app(app)
+	db.create_all()
 
 
 '''
@@ -23,18 +23,18 @@ Person
 Have title and release year
 '''
 class Person(db.Model):  
-  __tablename__ = 'People'
+	__tablename__ = 'People'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+	catchphrase = Column(String)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+	def __init__(self, name, catchphrase=""):
+		self.name = name
+		self.catchphrase = catchphrase
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+	def format(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'catchphrase': self.catchphrase}
