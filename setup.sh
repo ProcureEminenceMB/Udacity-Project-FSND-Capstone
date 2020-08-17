0e2df08d039df80f24d3d55c7addbcdeaf1aeaea
Michael Bigelow <procureeminence@gmail.com>
2020-08-17 16:57:29 -0400

    feat: Add export statement to each line

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
-AUTH0_DOMAIN='udacity-coffee-project.us.auth0.com'
-API_AUDIENCE='udacity-fsnd-capstone-project-api'
-CLIENT_ID='9Fv6meuLEH1FZeCcMz1HmqdZTqvjawyu'
-JWT_SECRET='DerpyCode'
-LOG_LEVEL=DEBUG
+#!/bin/sh
+
+export AUTH0_DOMAIN='udacity-coffee-project.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='udacity-fsnd-capstone-project-api'
+export CLIENT_ID='9Fv6meuLEH1FZeCcMz1HmqdZTqvjawyu'
+export JWT_SECRET='DerpyCode'
+export LOG_LEVEL='DEBUG'
